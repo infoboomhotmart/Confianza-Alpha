6ea4a2cfa2d6eeab4e981552e168b8ebcb3cbb4a
infoboomhotmart <infoboom2803@gmail.com>
2025-03-30 01:07:16 -0500

    Add files via upload

---
+++ js/timeruso.js
@@ -0,0 +1,13 @@
+const btn = document.getElementsByClassName("offer__a-btn");
+const txt = document.getElementsByClassName("offer__text");
+const offer = document.getElementsByClassName("offer");
+const comments = document.getElementsByClassName("face-comments");
+
+setTimeout(() => {
+  btn[0].style.display = "block";
+  btn[1].style.display = "block";
+  txt[0].style.display = "block";
+  txt[1].style.display = "block";
+  offer[0].style.display = "block";
+  comments[0].style.display = "block";
+}, 1603 * 1000);
